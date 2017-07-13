07de8d6823d7b277c5125a20568352d0308c121c
isaiastavares <isaiasengsoft@gmail.com>
2017-07-13 11:29:41 -0300

    Alterando alguns valores dos filmes padrão

--- src/main/java/br/com/loca/filmes/util/PopulaBanco.java
+++ src/main/java/br/com/loca/filmes/util/PopulaBanco.java
@@ -50,10 +50,10 @@ public class PopulaBanco {
         filme1.setTitulo("A Bela e a Fera");
         
         Calendar cFilme1 = Calendar.getInstance();
-        cFilme1.set(2017, 3, 16);
+        cFilme1.set(2017, 2, 16);
         filme1.setDataLancamento(cFilme1.getTime());
-        filme1.setDuracao(LocalTime.parse("01:20:34"));
-        filme1.setCensura(0);
+        filme1.setDuracao(LocalTime.parse("02:09:00"));
+        filme1.setCensura(10);
         filme1.setQuantidade(3);
         filmes.add(filme1);
 
@@ -61,9 +61,9 @@ public class PopulaBanco {
         filme2.setTitulo("Piratas do Caribe");
         
         Calendar cFilme2 = Calendar.getInstance();
-        cFilme2.set(2017, 5, 25);
+        cFilme2.set(2017, 4, 25);
         filme2.setDataLancamento(cFilme2.getTime());
-        filme2.setDuracao(LocalTime.parse("02:09:20"));
+        filme2.setDuracao(LocalTime.parse("02:09:00"));
         filme2.setCensura(12);
         filme2.setQuantidade(5);
         filmes.add(filme2);
@@ -72,9 +72,9 @@ public class PopulaBanco {
         filme3.setTitulo("A Múmia");
         
         Calendar cFilme3 = Calendar.getInstance();
-        cFilme3.set(2017, 3, 16);
+        cFilme3.set(2017, 5, 8);
         filme3.setDataLancamento(cFilme3.getTime());
-        filme3.setDuracao(LocalTime.parse("01:51:10"));
+        filme3.setDuracao(LocalTime.parse("01:51:00"));
         filme3.setCensura(12);
         filme3.setQuantidade(2);
         filmes.add(filme3);
